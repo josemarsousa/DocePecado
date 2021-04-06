156b8801183148cac1bc1eab71b963c480b7b4b5
Josemar <josemaar.sousa@gmail.com>
2021-04-06 18:39:24 -0300

    Client Controller - GetAll

---
+++ Back/DocePecado.API/Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using DocePecado.Application.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocePecado.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var clients = await _clientService.GetAllClientsAsync();
+                if (clients == null) return NotFound();
+
+                return Ok(clients);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar clientes. Erro: {ex.Message}");
+            }
+        }
+    }
+}
